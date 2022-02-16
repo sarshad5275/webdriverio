3f650c43e9bebf973576e4e13e039ac82597a9ba
CORP\svalluri <svalluri@nexient.com>
2022-02-16 16:46:15 -0500

    DB test added for creating connection

--- test/common/DBCommon.js
+++ test/common/DBCommon.js
@@ -1,50 +1,56 @@
+const mysql = require('mysql');
+import Reporter from '../common/Reporter';
+import Asserts from './Asserts';
+
 class DBCommon{
 
-    async dbConnection(hostURL, DBUserName, DBPassword,SQLQuery){
-        const mysql = require('mysql');
+    async dbConnect(hostURL, DBUserName, DBPassword){
         const dbcon = mysql.createConnection({
         host: hostURL,
         user: DBUserName,
         password: DBPassword
         });
-        dbcon.connect(function(err) {
-        if (err) {
-            console.log("Connection failed. Please check");
-            throw err;
-            }
+        dbcon.connect(function(error) {
+        if (error) {
+        console.log("DB Connection failed. Please check the input parameters");
+        Reporter.addStep("DB Connection failed. Please check the input parameters");
+        }else{
         console.log("DB Connection successfull");
-        con.query(SQLQuery, function (err, result) {
-            if (err) {
-            console.log("Query failed. Please check");
-            throw err;
-            }
-            console.log("Query result is " + result);
-            return result;
-          });
+        Reporter.addStep("DB Connection Successful");
+        return dbcon;
+        }
         })
     }
 
+    async dbQuery(dbconnect, SQLQuery){
+        dbconnect.query(SQLQuery, function (error, result) {
+            if (error) {
+            console.log("Please check the Query again");
+            dbconnect.destroy;
+            return false;
+            }
+            console.log("Result from the Query Execution : " + result);
+            dbconnect.destroy;
+            return result;
+          });
+    }
+    
     async databaseCreation(){
     var con = mysql.createConnection({
         host: "localhost",
-        user: "testing",
-        password: "password"
+        user: "root",
+        password: "password",
+        port:"3306"
       });
       
       con.connect(function(err) {
         if (err) throw err;
-        console.log("Connected!");
-        con.query("CREATE DATABASE TestDB", function (err, result) {
+        console.log("Database Connection successful");
+        con.query("CREATE DATABASE QAAutomationDB", function (err, result) {
           if (err) throw err;
           console.log("Database created");
         });
       });
-    }
-
-    async db(){
-        var connection = sql.connect(config); //config has all the parameters
-        var request = new sql.Request();
-        request.query('select * from myTable');
     }
 
     result = dbConnection();
