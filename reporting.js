a8167b778b76ef95c9ed0c56a254993b6261a614
CORP\svalluri <svalluri@nexient.com>
2022-02-28 17:22:14 -0500

    cucumber html reporting for multiple jsonfiles

--- reporting.js
+++ reporting.js
@@ -1,22 +1,36 @@
-var reporter = require('cucumber-html-reporter');
+const report = require('multiple-cucumber-html-reporter');
 
-var options = {
-        theme: 'bootstrap',
-        jsonFile: 'features/cucumber-reports/json',
-        output: 'features/cucumber-reports/cucumber_report.html',
-        screenshotsDirectory: 'features/cucumber-reports/screenshots/',
-        storeScreenshots: true,
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true,
-        launchReport: true,
-        metadata: {
-           // "App Version":"0.3.2",
-            "Test Environment": "WebDriverIO Automation",
-            "Browser": "Chrome",
-           // "Platform": "Windows 10",
-           // "Parallel": "Scenarios",
-            "Executed": "local"
+report.generate({
+	jsonDir: 'features/cucumber-reports/json/',
+	reportPath: 'features/cucumber-reports/',
+    openReportInBrowser:true,
+    staticFilePath:true,
+    saveCollectedJSON:true,
+    pageTitle:'CucumberReport',
+    reportName:'CucumberReport',
+    pageFooter:'Developed by Nexient LLC',
+    displayDuration:true,
+    hideMetadata:true,
+    displayReportTime:true,
+	metadata:{
+        browser: {
+            name: 'chrome',
+            version: 'Latest'
+        },
+        device: 'My Laptop',
+        platform: {
+            name: 'Windows',
+            version: 'Latest'
         }
-    };
-
-    reporter.generate(options);
+    },
+    customData: {
+        title: 'WebDriverIO Automation',
+        data: [
+            {label: 'Project', value: 'WebDriverIO Automation project'},
+            {label: 'Release', value: '1'},
+            {label: 'Cycle', value: '1'}
+           // {label: 'Execution Start Time', value: 'Nov 19th 2017, 02:31 PM EST'},
+           // {label: 'Execution End Time', value: 'Nov 19th 2017, 02:56 PM EST'}
+        ]
+    }
+});
